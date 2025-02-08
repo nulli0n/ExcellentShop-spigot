1d228e6d982a0325e283107f601da1a7f50d28f9
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2025-02-08 12:40:21 +0100

    Add more auction placeholders to PlaceholderAPI hook

--- Core/src/main/java/su/nightexpress/nexshop/hook/PlaceholderHook.java
+++ Core/src/main/java/su/nightexpress/nexshop/hook/PlaceholderHook.java
@@ -3,8 +3,12 @@ package su.nightexpress.nexshop.hook;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import su.nightexpress.economybridge.EconomyBridge;
+import su.nightexpress.economybridge.api.Currency;
 import su.nightexpress.nexshop.ShopPlugin;
 import su.nightexpress.nexshop.auction.AuctionManager;
+import su.nightexpress.nexshop.auction.listing.CompletedListing;
 import su.nightexpress.nexshop.shop.chest.ChestShopModule;
 import su.nightexpress.nexshop.shop.chest.ChestUtils;
 import su.nightexpress.nexshop.shop.virtual.VirtualShopModule;
@@ -61,18 +65,76 @@ public class PlaceholderHook {
 
         @Override
         public String onPlaceholderRequest(Player player, @NotNull String params) {
-            if (player == null) return null;
-
             if (params.startsWith("auction_")) {
                 AuctionManager module = this.plugin.getAuction();
                 if (module == null) return null;
 
                 String subParams = params.substring("auction_".length());
 
+                if (subParams.equalsIgnoreCase("all_active_listings")) {
+                    return NumberUtil.format(module.getListings().getActive().size());
+                }
+                if (subParams.equalsIgnoreCase("all_completed_listings")) {
+                    return NumberUtil.format(module.getListings().getCompleted().size());
+                }
+
+                if (player == null) return null;
+
                 if (subParams.equalsIgnoreCase("max_listings")) {
                     return NumberUtil.format(module.getListingsMaximum(player));
                 }
+                if (subParams.equalsIgnoreCase("active_listings")) {
+                    return NumberUtil.format(module.getListings().getActive(player).size());
+                }
+                if (subParams.equalsIgnoreCase("unclaimed_listings")) {
+                    return NumberUtil.format(module.getListings().getUnclaimed(player).size());
+                }
+                if (subParams.equalsIgnoreCase("expired_listings")) {
+                    return NumberUtil.format(module.getListings().getExpired(player).size());
+                }
+                if (subParams.equalsIgnoreCase("claimed_listings")) {
+                    return NumberUtil.format(module.getListings().getClaimed(player).size());
+                }
+                if (subParams.startsWith("unclaimed_income_raw_")) {
+                    String currency = subParams.substring("unclaimed_income_raw_".length());
+                    return String.valueOf(
+                            module.getListings().getUnclaimed(player).stream()
+                                    .filter(listing -> listing.getCurrency().getInternalId().equalsIgnoreCase(currency))
+                                    .mapToDouble(CompletedListing::getPrice)
+                                    .sum()
+                    );
+                }
+                if (subParams.startsWith("unclaimed_income_")) {
+                    Currency currency = EconomyBridge.getCurrency(subParams.substring("unclaimed_income_".length()));
+                    return (currency == null) ? null : currency.format(
+                            module.getListings().getUnclaimed(player).stream()
+                                    .filter(listing -> listing.getCurrency().getInternalId().equalsIgnoreCase(currency.getInternalId()))
+                                    .mapToDouble(CompletedListing::getPrice)
+                                    .sum()
+                    );
+                }
+                if (subParams.startsWith("claimed_income_raw_")) {
+                    String currency = subParams.substring("claimed_income_raw_".length());
+                    return String.valueOf(
+                            module.getListings().getClaimed(player).stream()
+                                    .filter(listing -> listing.getCurrency().getInternalId().equalsIgnoreCase(currency))
+                                    .mapToDouble(CompletedListing::getPrice)
+                                    .sum()
+                    );
+                }
+                if (subParams.startsWith("claimed_income_")) {
+                    Currency currency = EconomyBridge.getCurrency(subParams.substring("claimed_income_".length()));
+                    return (currency == null) ? null : currency.format(
+                            module.getListings().getClaimed(player).stream()
+                                    .filter(listing -> listing.getCurrency().getInternalId().equalsIgnoreCase(currency.getInternalId()))
+                                    .mapToDouble(CompletedListing::getPrice)
+                                    .sum()
+                    );
+                }
             }
+
+            if (player == null) return null;
+
             else if (params.startsWith("chestshop_")) {
                 ChestShopModule module = this.plugin.getChestShop();
                 if (module == null) return null;
